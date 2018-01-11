84eae1295bd360946d2fcaa7f46c73eaaf3cb535
wanghaha <2466543685@qq.com>
2018-01-11 12:41:43 +0800

    添加项目文件。

---
+++ Work_Submit/MainModelSum.cs
@@ -0,0 +1,146 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+using System.Xml;
+using System.Xml.Linq;
+
+namespace Work_Submit
+{
+    class MainModelSum: INotifyPropertyChanged
+    {
+        public string Pattern
+        {
+            get { return _Pattern; }
+            set
+            {
+                if (_Pattern == value) return; _Pattern = value; OnPropertyChanged(nameof(Pattern));
+            }
+        }
+        private string _Pattern;
+
+        public string ReplacePattern { get { return _ReplacePattern; } set { if (_ReplacePattern == value) return; _ReplacePattern = value; OnPropertyChanged(nameof(ReplacePattern)); } }
+        private string _ReplacePattern;
+
+        public string TargetText { get { return _TargetText; } set { if (_TargetText == value) return; _TargetText = value; OnPropertyChanged(nameof(TargetText)); } }
+        private string _TargetText;
+
+        public List<MatchView> Matches { get { return _Matches; } private set { if (_Matches == value) return; _Matches = value; OnPropertyChanged(nameof(Matches)); } }
+        private List<MatchView> _Matches;
+
+        public string CollectedText { get { return _CollectedText; } private set { if (_CollectedText == value) return; _CollectedText = value; OnPropertyChanged(nameof(CollectedText)); } }
+        private string _CollectedText;
+
+        public string ReplaceResult { get { return _ReplaceResult; } private set { if (_ReplaceResult == value) return; _ReplaceResult = value; OnPropertyChanged(nameof(ReplaceResult)); } }
+        private string _ReplaceResult;
+
+        private static Encoding[] _Encodings = new Encoding[] { Encoding.Default, Encoding.ASCII, Encoding.UTF8, Encoding.Unicode, Encoding.UTF7, Encoding.UTF32, Encoding.BigEndianUnicode };
+        public Encoding[] Encodings { get { return _Encodings; } }
+
+        public Encoding CurrentEncoding { get { return _CurrentEncoding; } set { if (_CurrentEncoding == value) return; _CurrentEncoding = value; OnPropertyChanged(nameof(CurrentEncoding)); } }
+        private Encoding _CurrentEncoding = Encoding.Default;
+
+        public bool CanStartMatch
+        {
+            get
+            {
+                return !string.IsNullOrWhiteSpace(Pattern) && !string.IsNullOrWhiteSpace(TargetText);
+            }
+        }
+
+        public bool CanStartReplace
+        {
+            get
+            {
+                return !string.IsNullOrWhiteSpace(Pattern) && !string.IsNullOrWhiteSpace(ReplacePattern) && !string.IsNullOrWhiteSpace(TargetText);
+            }
+        }
+
+        public void LoadTargetTextFrom(string aFileName)
+        {
+            TargetText = File.ReadAllText(aFileName, CurrentEncoding);
+        }
+
+        public class GroupView
+        {
+            public int Index { get; set; }
+            public string Value { get; set; }
+        }
+        public class MatchView
+        {
+            public int Index { get; set; }
+            public string Value { get; set; }
+            public List<GroupView> Groups { get; set; }
+        }
+        public void GetMatches()
+        {
+            Regex aRegex = new Regex(Pattern);
+            MatchCollection aMatches = aRegex.Matches(TargetText);
+            List<MatchView> aMatchViews = new List<MatchView>();
+            for (int i = 0; i < aMatches.Count; i++)
+            {
+                List<GroupView> aGroupViews = new List<GroupView>();
+                for (int j = 0; j < aMatches[i].Groups.Count; j++)
+                {
+                    aGroupViews.Add(new GroupView { Index = j, Value = aMatches[i].Groups[j].Value });
+                }
+                aMatchViews.Add(new MatchView { Index = i, Value = aMatches[i].Value, Groups = aGroupViews });
+            }
+            Matches = aMatchViews;
+
+            StringBuilder aStringBuilder = new StringBuilder();
+            foreach (Match aMatch in aMatches)
+            {
+                aStringBuilder.AppendLine(aMatch.Value);
+            }
+            CollectedText = aStringBuilder.ToString();
+        }
+
+        public void Replace()
+        {
+            Regex aRegex = new Regex(Pattern);
+            ReplaceResult = aRegex.Replace(TargetText, ReplacePattern);
+        }
+
+        private void OnPropertyChanged(string aPropertyName)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(aPropertyName));
+        }
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        public XmlDocument CurrentXml { get { return _CurrentXml; } set { if (_CurrentXml == value) return; _CurrentXml = value; OnPropertyChanged(nameof(CurrentXml)); } }
+        private XmlDocument _CurrentXml;
+        private string _FileName;
+
+        public void LoadXmlFile(string aFileName)
+        {
+            XmlDocument aXml = new XmlDocument();
+            aXml.Load(aFileName);
+            CurrentXml = aXml;
+            _FileName = aFileName;
+        }
+        private const string ConfigFileName = "xmllesson01.cfg";
+        public void SaveConfig()
+        {
+            XDocument aXDocument = new XDocument(
+                new XElement("Config",
+                    new XElement("LastFile", _FileName)
+                )
+            );
+            aXDocument.Save(ConfigFileName);
+        }
+        public void LoadConfig()
+        {
+            XDocument aXDocument = XDocument.Load(ConfigFileName);
+            _FileName = aXDocument.Root.Element("LastFile").Value;
+            LoadXmlFile(_FileName);
+        }
+
+
+
+    }
+}
---
+++ Work_Submit/MainWindow.xaml.cs
@@ -0,0 +1,104 @@
+﻿using Microsoft.Win32;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Work_Submit
+{
+    /// <summary>
+    /// MainWindow.xaml 的交互逻辑
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+            _Model = new MainModelSum();
+            this.DataContext = _Model;
+        }
+        private MainModelSum _Model;
+        private string FileName;
+        //打开XMl文档操作
+        private void OnOpenFile_Exedcuted(object sender, ExecutedRoutedEventArgs e)
+        {
+            try
+            {
+                OpenFileDialog aDlg = new OpenFileDialog();
+                aDlg.Filter = "文本文件|*.xml;*.txt;*.html;*.log|所有文件|*.*";
+                if (aDlg.ShowDialog() != true) return;
+                FileName = aDlg.FileName;
+                _Model.LoadTargetTextFrom(aDlg.FileName);
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+        }
+
+        //判断是否可执行打开XML文档操作
+        private void OnOpenFile_CanExecute(object sender, CanExecuteRoutedEventArgs e)
+        {
+            e.CanExecute = true;
+        }
+
+        //查询Xml文档操作
+        private void OnQueryXml_Exedcuted(object sender, ExecutedRoutedEventArgs e)
+        {
+            try
+            {
+                _Model.GetMatches();
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+        }
+        //判断是否可执行查询Xml文档操作
+        private void OnQueryXml_CanExecute(object sender, CanExecuteRoutedEventArgs e)
+        {
+            e.CanExecute = true;
+        }
+
+        //解析Xml文档
+        private void AnalysisXml_Executed(object sender, ExecutedRoutedEventArgs e)
+        {
+            try
+            {
+                _Model.LoadXmlFile(FileName);
+            }
+            catch (Exception msg)
+            {
+                MessageBox.Show(msg.Message);
+            }
+
+        }
+        //判断是否解析Xml文档
+        private void AnalysisXml_CanExedcuted(object sender, CanExecuteRoutedEventArgs e)
+        {
+            e.CanExecute = true;
+        }
+
+        //保存到Xml文档
+        private void SaveToSql_Executed(object sender, ExecutedRoutedEventArgs e)
+        {
+
+        }
+
+        //判断是否保存到Xml文档
+        private void SaveToSql_CanExecuted(object sender, CanExecuteRoutedEventArgs e)
+        {
+            e.CanExecute = true;
+        }
+    }
+}
